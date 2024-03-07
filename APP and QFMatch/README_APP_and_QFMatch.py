8ecd7abb2e3cb23cb0ba6841772ff85c9d1d2f3f
Evgeniy Tabatsky <tabatsky.evgeniy@gene.com>
2024-03-07 19:13:04 +1000

    README_APP_and_QFMatch.py updated

--- APP and QFMatch/README_APP_and_QFMatch.py
+++ APP and QFMatch/README_APP_and_QFMatch.py
@@ -1,4 +1,5 @@
 import subprocess
+from pathlib import Path
 
 min_cluster_size = 300
 ndim = 10
@@ -17,13 +18,13 @@ fn_in = 'test_data/PHA0026.csv'
 # step 1:
 
 # APP variant 1.A (parallel, with default workers count):
-# subprocess.run(["python", "APP/variation_parallel_from_cmd.py",
+# subprocess.run(["python", "APP/variation_parallel_from_cmd_fixed_param.py",
 #                 fn_in, str(min_cluster_size), str(ndim)])
 # APP variant 1.B (parallel, with custom workers count):
-subprocess.run(["python", "APP/variation_parallel_from_cmd.py",
+subprocess.run(["python", "APP/variation_parallel_from_cmd_fixed_param.py",
                 fn_in, str(min_cluster_size), str(ndim), str(workers_count)])
 # APP variant 2 (consistent):
-# subprocess.run(["python", "APP/variation_from_cmd.py",
+# subprocess.run(["python", "APP/variation_from_cmd_fixed_param.py",
 #                 fn_in, str(min_cluster_size), str(ndim)])
 
 
@@ -32,15 +33,19 @@ subprocess.run(["python", "APP/variation_parallel_from_cmd.py",
 # calculates phenograph and supervised UMAP by ground truth:
 subprocess.run(["python", "dml_for_APP/dml_from_cmd.py", fn_in])
 
+fn_in = Path(fn_in).stem + '.csv'
 
 # step 3.A.A:
 # asymmetric QFMatch with black misclassified cells:
 
 # between ground truth and APP:
-# with custom filter size:
+# with custom filter size and custom workers count:
 subprocess.run(["python", "QFMatch_asymmetric/QFMatch_parallel_from_cmd_asymmetric.py",
-                fn_in, '0', str(bin_size), str(ndim), str(filter_size)])
-# with default (zero) filter size:
+                fn_in, '0', str(bin_size), str(ndim), str(filter_size), str(workers_count)])
+# with custom filter size and default workers count:
+# subprocess.run(["python", "QFMatch_asymmetric/QFMatch_parallel_from_cmd_asymmetric.py",
+#                 fn_in, '0', str(bin_size), str(ndim), str(filter_size)])
+# with default (zero) filter size and default workers count:
 # subprocess.run(["python", "QFMatch_asymmetric/QFMatch_parallel_from_cmd_asymmetric.py",
 #                 fn_in, '0', str(bin_size), str(ndim)])
 # between ground truth and phenograph:
@@ -53,10 +58,13 @@ subprocess.run(["python", "QFMatch_asymmetric/QFMatch_parallel_from_cmd_asymmetr
 # asymmetric QFMatch without black misclassified cells:
 
 # between ground truth and APP:
-# with custom filter size:
+# with custom filter size and custom workers count:
 subprocess.run(["python", "QFMatch_asymmetric/QFMatch_parallel_from_cmd_asymmetric_no_black.py",
-                fn_in, '0', str(bin_size), str(ndim), str(filter_size)])
-# with default (zero) filter size:
+                fn_in, '0', str(bin_size), str(ndim), str(filter_size), str(workers_count)])
+# with custom filter size and default workers count:
+# subprocess.run(["python", "QFMatch_asymmetric/QFMatch_parallel_from_cmd_asymmetric_no_black.py",
+#                 fn_in, '0', str(bin_size), str(ndim), str(filter_size)])
+# with default (zero) filter size and default workers count:
 # subprocess.run(["python", "QFMatch_asymmetric/QFMatch_parallel_from_cmd_asymmetric_no_black.py",
 #                 fn_in, '0', str(bin_size), str(ndim)])
 # between ground truth and phenograph:
