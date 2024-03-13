7aa43adf6f462d4e26d268c489bb5f689cc5e8de
Evgeniy Tabatsky <tabatsky.evgeniy@gene.com>
2024-03-14 05:12:20 +1000

    some descriptions added

--- APP and Label Transfer/QFMatch_symmetric/QFMatch_from_cmd_with_clusters_visualization.py
+++ APP and Label Transfer/QFMatch_symmetric/QFMatch_from_cmd_with_clusters_visualization.py
@@ -11,6 +11,13 @@ right file. Bin the mix, then separate the mix into pairs
 "bin grid" for each of the pairs and calculate dissimilarities using quadratic
 form based comparison.
 Unmatched clusters are attempted to be merged to the originally matched ones.
+
+The script executes many clusters-to-many cluster matching with QFMatch between
+the test set and training set results of the distance metric learning script.
+Further  visualizes the matching outcomes and computes the misclassification
+rate (if this task is required). Data points that are misclassified
+are visually highlighted by being painted black.
+Also visualizes the original clusters.
 """
 
 import os
